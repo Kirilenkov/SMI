f1e71de81f778a30cb60d81525152fbe210b54cf
Kirill <kkirilenkov@gmail.com>
2019-10-31 05:36:09 +0700

    averaging v 0.0.2

--- core.py
+++ core.py
@@ -1,6 +1,12 @@
 import os
 import pandas as pd
-from difflib import ndiff
+
+# for new averaging methods:
+# from difflib import ndiff
+'''Максимальное количество файлов в папке для анализа:'''
+MAX_FILES = 60
+ORDINARY_PATH = 'C:/Users/Kirill/Desktop/Test_export'
+writer = None
 
 
 class AppExc(Exception):
@@ -19,45 +25,71 @@ class PandasExceptions(Exception):
     pass
 
 
-path = 'C:/Users/Kirill/Desktop/Test_export'
-os.chdir(path)
-writer = pd.ExcelWriter('test.xlsx')
+msg = 'Enter the full path to the folder with the SMI export files: \n'
+
+
+def path_setter(link, message=msg, stage=False):
+    if message[-1] != '\n':
+        message += '\n'
+    try:
+        os.chdir(link)
+    except FileNotFoundError:
+        if not stage:
+            print('Hard path not found')
+        else:
+            print('Cannot find the specified path')
+        path_setter(input(message), message=message, stage=True)
+
 
 def slash_reflector(file_name):
-    true_file_name = file_name.replace('\\', '/')
+    if os.name == 'nt':
+        true_file_name = file_name.replace('\\', '/')
+    else:
+        true_file_name = file_name.replace('/', '\\')
     return true_file_name
 
 
 def check_txt_ext(file_list):
-    if len(file_list) < 60:  # Настроить предельное количество файлов
+    if len(file_list) < MAX_FILES:
         bool_storage = False
-        # must be while with condition (not bool_storage and i < len(file_list))
         for file in file_list:
-            bool_storage = bool_storage or '.txt' in file
+            if '.txt' in file:
+                bool_storage = True
+                break
         if not bool_storage:
             raise ExcDict['WithoutTxtFiles']
     else:
         raise ExcDict['TooManyFiles']
 
 
-def add_txt(file_list, path):
-    path = slash_reflector(path)
-    slash = ''
-    if path[-1] != '/':
-        slash = '/'
+def slash_checker(path_to_dir):
+    if os.name == 'nt':
+        if path_to_dir[-1] != '/':
+            return '/'
+    else:
+        if path_to_dir[-1] != '\\':
+            return '\\'
+    return ''
+
+
+def add_txt(file_list, path_to_dir):
+    path_to_dir = slash_reflector(path_to_dir)
+    slash = slash_checker(path_to_dir)
     output_list = []
     for file in file_list:
         if '.txt' in file:
-            file = path + slash + file
+            file = path_to_dir + slash + file
             output_list.append(file)
     return output_list
 
 
 def check_of_adding():
+    # for test boosting:
     # return True
     while True:
         stop_continue = str(input("Do you want add more files? y/n: \n"))
         if stop_continue.lower() == 'y':
+            path_setter('-', stage=True)
             return False
         elif stop_continue.lower() == 'n':
             return True
@@ -71,29 +103,6 @@ def data_length_checker(data):
         return False
     else:
         return True
-
-
-def define_time():
-    while True:
-        try:
-            print('Установите временной интервал для анализа данных в мс.')
-            # start = int(input('Время начала: '))
-            # stop = int(input('Время окончания: '))
-            start = 0
-            stop = 3000
-            if stop < start:
-                raise ExcDict['FlipValues']
-        except ValueError:
-            print('Введите целочисленные данные')
-        except Exception as e:
-            print(e)
-        else:
-            break
-
-    def time_int():
-        return start, stop
-
-    return time_int
 
 
 def time_check_up_decorator(start_time, end_time):
@@ -125,7 +134,7 @@ def time_check_up_decorator(start_time, end_time):
 def binding_with_time(file_list, start_time, stop_time, *args):
     output_file_list = []
     slash = ''
-    path = ''
+    path_to_dir = ''
 
     @time_check_up_decorator(start_time, stop_time)
     def open_csv(data_file):
@@ -133,9 +142,8 @@ def binding_with_time(file_list, start_time, stop_time, *args):
 
     # must be try-except:
     if len(args) > 0:
-        path = slash_reflector(args[0])
-        if path[-1] != '/':
-            slash = '/'
+        path_to_dir = slash_reflector(args[0])
+        slash = slash_checker(path_to_dir)
     for file in file_list:
         trigger = False
         if '.txt' in file:
@@ -144,21 +152,41 @@ def binding_with_time(file_list, start_time, stop_time, *args):
             except PandasExceptions as e:
                 print(e)
         if trigger:
-            file = path + slash + file
+            file = path_to_dir + slash + file
             output_file_list.append(file)
     return output_file_list
+
+
+def define_time():
+    while True:
+        try:
+            print('Установите временной интервал для анализа данных в мс.')
+            start = int(input('Время начала: '))
+            stop = int(input('Время окончания: '))
+            if stop < start:
+                raise ExcDict['FlipValues']
+        except ValueError:
+            print('Введите целочисленные данные')
+        except Exception as e:
+            print(e)
+        else:
+            break
+
+    def time_int():
+        return start, stop
+
+    return time_int
 
 
 def input_console_data(time):
     data = []
     start_time, stop_time = time()
+    path = ORDINARY_PATH
+    path_setter(path)
+    global writer
+    writer = pd.ExcelWriter('test.xlsx')
     while True:
-        print("Specify the full path to the data folder")
-        print("Example 'X:/Users/MySMIexportFolder': ")
-        # path = str(input())
-        path = 'C:/Users/Kirill/Desktop/Test_export'
         try:
-            os.chdir(path)
             check_txt_ext(os.listdir('.'))
             relevant_files = binding_with_time(os.listdir('.'), start_time, stop_time, path)
         except FileNotFoundError:
@@ -191,9 +219,9 @@ def add_suff(trial):
     trial_suffix = {2: '_M_NF', 4: '_M_HF_f', 10: '_M_AF_f', 8: '_Self',
                     6: '_F_SF_f', 20: '_F_HF_f', 16: '_Self', 18: '_F_AF_f',
                     12: '_F_NF', 14: '_M_SF_f'}
-    trnum = int(trial[-2:])
-    if trnum in trial_suffix:
-        suffix = trial_suffix[trnum]
+    tr_num = int(trial[-2:])
+    if tr_num in trial_suffix:
+        suffix = trial_suffix[tr_num]
     return suffix
 
 
@@ -256,9 +284,8 @@ def strings_df(data_frame, time):
     dflist_hor = []
     ln = data_frame.__len__()
     ln_aoi = len(data_frame.columns) - 7
-    for i in range(ln):
-        # С loc могут возникнуть проблемы iloc надёжнее
 
+    for i in range(ln):
         par = data_frame.iloc[i, 4]
         if i == 0:
             # adding headers:
@@ -288,7 +315,10 @@ def strings_df(data_frame, time):
 def main(file_list, time):
     ln = len(file_list)
     # Must be refactored for GUI version:
-    print(f'Amount of relevant files: {ln}', file_list, '\n')
+    print(f'Amount of relevant files: {ln}')
+    for file in file_list:
+        print(file)
+
     dflist = []
     for f in file_list:
         dflist.append(open_csv(f))
@@ -299,8 +329,6 @@ def main(file_list, time):
     tr = list(commondf.columns).index('Trial')
     aois = list(commondf.columns).index('AOI Name')
     for i in range(len(commondf.index)):
-        # Какие то проблемы с индексацией loc по названию столбцов.
-        # Значения с OUT почему-то пропускаются.
         trial = commondf.iloc[i, tr]
         aoi = commondf.iloc[i, aois]
         if int(trial[-1]) % 2 != 0 or 'OUT' in aoi:
